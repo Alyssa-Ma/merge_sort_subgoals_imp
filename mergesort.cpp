7c04eb65d6939d7c3076670f5e50e6eaeaf30eee
ama3489 <Alyssa.Ma80@myhunter.cuny.edu>
2019-03-10 21:05:38 -0400

    Setting up funcs

--- mergesort.cpp
+++ mergesort.cpp
@@ -1,19 +1,54 @@
 #include <iostream>
 #include <vector>
 
-//Find the size of the vector
+void merge_sort(vector<int> vec)
+{
+    //Find the size of the vector
+    int s = vec.size();
+    //Find the middle of the vector
+    int h = s / 2;
+
+    //Sort the first half of the vector
 
 
-//Find the middle of the vector
+    //Sort the second half of the vector
 
 
-//Sort the first half of the vector
+    //Merge the two sorted halves back together
 
 
-//Sort the second half of the vector
+    //Continue until entire vector is sorted
+}
 
+//helper function for merge sort
+void merge_helper(vector<int> vec)
+{
+    int currmin = 0; //current min index
+    int currmax = vec.size() - 1 //current max index
+}
 
-//Merge the two sorted halves back together
+//print function for testing purposes
+void print_vec(vector<int> vec)
+{
+    int s = vec.size();
+    for(int i = 0; i < s; i++)
+    {
+        std::cout << vec[i] << " ";
+    }
+    std::cout << "\n";
+}
 
+int main()
+{
+    vector<int> vec(20);
 
-//Continue until entire vector is sorted
+    //for a random vector
+    for(int i = 0; i < 20; i++)
+    {
+        vec[i] = rand() % 100;
+    }
+
+    merge_sort(vec);
+
+    return 0;
+}
