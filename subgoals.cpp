de5dd447597398df4da12e6dcecd8c53953d7f82
ama3489 <Alyssa.Ma80@myhunter.cuny.edu>
2019-03-10 20:49:42 -0400

    Finished subgoals

--- subgoals.cpp
+++ subgoals.cpp
@@ -1,4 +1,19 @@
 #include <iostream>
 #include <vector>
 
+//Find the size of the vector
 
+
+//Find the middle of the vector
+
+
+//Sort the first half of the vector
+
+
+//Sort the second half of the vector
+
+
+//Merge the two sorted halves back together
+
+
+//Continue until entire vector is sorted
